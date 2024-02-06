a72d8b68ddfd12e64148e33c3f5865f3962afd73
Pinchai <>
2024-02-06 08:12:56 +0700

    demo lesson 1

---
+++ lesson/variable.py
@@ -0,0 +1,41 @@
+# Creating Variables
+# age = 10
+# print(type(age))
+# age = 10.1
+# print(type(age))
+# age = "hello"
+# print(type(age))
+# age = [1, 2, 3]
+# print(type(age))
+
+# Casting
+# age = str(10)
+# print(type(age))
+
+# age = int(10.52)
+# print(age)
+
+# age = float(10)
+# print(type(age))
+
+# One Value to Multiple Variables
+# x, y, z = 10, 20, 'hello'
+# print(x, y, z)
+
+# Unpack a Collections
+# student_list = ['sopheak', 'theara', 'vanthy']
+# std1, std2, std3 = student_list
+# print(std1, std2, std3)
+
+
+
+
+
+
+
+
+
+
+
+
+
