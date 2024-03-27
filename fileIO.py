f80a740af2474d6ffa425e0bd6a617546337cc8c
Pinchai <>
2024-03-27 08:26:11 +0700

    file Io

---
+++ fileIO.py
@@ -0,0 +1,24 @@
+#r = read,
+# f = open('file.txt', 'r')
+# print(f.read())
+# print(f.readline())
+#print(f.readlines())
+# f.close()
+
+# ==============
+# w = write,
+# a = append
+# for i in range(1, 11):
+#     fw = open('file.txt', 'a')
+#     fw.write(f"Setec {i} \n")
+
+# 1. insert
+# 2. update (get file by Index)
+# 3. delete (Delete by Index)
+# 4. view (print all record in text file)
+
+
+
+
+
+
